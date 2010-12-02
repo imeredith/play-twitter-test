821f1e88b6782627ca64e8e75df1635160fb9635
Ivan Meredith <imeredith@cloudbees.com>
2010-12-02 13:15:18 +1300

    Added some css

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -11,10 +11,20 @@ import org.w3c.dom.Document;
 import models.*;
 
 public class Application extends Controller {
-
-    public static void index() {
-    	String results = WS.url("http://search.twitter.com/search.atom?q=cloudbees").get().getString();
-        render(results);
+	
+	public static void index(){
+		search("cloudbees");
+	}
+    public static void search(String search) {
+    	if(search == null || search.length() == 0){
+    		search = "cloudbees";
+    	}
+    	String results = WS.url("http://search.twitter.com/search.atom?q="+WS.encode(search)).get().getString();
+        render(results, search);
+    }
+    
+    public static void postSearch(String search){
+    	search(WS.encode(search));
     }
 
 }
